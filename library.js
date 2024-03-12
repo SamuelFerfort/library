e855fe2b2e7d1cda1818e510c107e704df5b167c
Samuel <samuferfort@gmail.com>
2024-03-12 13:46:37 +0100

    Added delete and status buttons

--- library.js
+++ library.js
@@ -1,20 +1,20 @@
 const myLibrary = [];
 const bookForm = document.querySelector("#submitBtn");
 const table = document.querySelector("#table")
+const buttons = document.querySelectorAll("button");
 
-function Book(title, author, pages, yes) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = yes === "yes"
-    ? "read" : "not read yet";
+    this.read = status === "yes"
+    ? "yes" : "no";
     this.info = function sayInfo() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 
 }
-const mistborn = new Book("Mistborn", "Brandon Sanderson", "669", "yes");
-myLibrary.push(mistborn)
+
 
 function addBookToLibrary(e) {
     e.preventDefault();
@@ -40,20 +40,48 @@ function addBookToLibrary(e) {
 bookForm.addEventListener("click", addBookToLibrary);
 
 
-console.log(myLibrary);
+
 function displayBooks() {
     table.innerHTML = ""; // Clear the table before populating it
 
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
         const row = document.createElement("tr");
+        
+        
         row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.pages}</td>
             <td>${book.read}</td>
+            
         `;
+        
+        // Create a delete button using index
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+            myLibrary.splice(index, 1);
+            displayBooks();
+        })
+        
+        // Change read status button
+        const updateReadBtn = document.createElement("button");
+        updateReadBtn.textContent = "Status";
+        
+        updateReadBtn.addEventListener("click", () => {
+            if (book.read == "yes") {
+                book.read = "no"
+            } else {
+                book.read = "yes"
+            }
+            displayBooks()
+        })
+
         table.appendChild(row);
+        table.appendChild(deleteButton);
+        table.appendChild(updateReadBtn)
     });
+        
 }
 
-displayBooks();
+
